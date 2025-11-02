a72a5d6835ef49e1e7a7c921cf8dda1e67192554
Amantrubleshooter <117078339+Amantrubleshooter@users.noreply.github.com>
2025-11-02 12:34:05 +0530

    chnageing setting.py4

--- pr1/settings.py
+++ pr1/settings.py
@@ -1,136 +1,25 @@
 
-########above is my inial hosting code code
-
-"""
-LOCAL Development Settings - Uses Aman database on localhost
-"""
-import os
-from pathlib import Path
-
-# Build paths
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Security
-SECRET_KEY = 'django-insecure-^#ku(-=1nwd8wnfe&1eri+k)_(8g@!n*#p3&!i4=!!c)&rhl3v'
-DEBUG = True
-ALLOWED_HOSTS = ['*']
-
-# Timezone - CONSISTENT!
-USE_TZ = True
-TIME_ZONE = 'Asia/Kolkata'
-
-# Application definition
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.humanize',
-    'cafe',
-    'django_crontab',
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-ROOT_URLCONF = 'pr1.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates', 'static'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'pr1.wsgi.application'
-
-# Database - LOCAL PostgreSQL
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Aman',              # Local database name
-        'USER': 'postgres',          # Local DB username
-        'PASSWORD': '123',           # Local DB password
-        'HOST': 'localhost',         # Local host
-        'PORT': '5432',              # PostgreSQL port
-    }
-}
-
-# Password validation
-AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
-]
-
-# Internationalization
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'Asia/Kolkata'
-USE_I18N = True
-USE_TZ = False  # IMPORTANT: Consistent with top!
-
-# Static files
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Media files - LOCAL storage
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-
-# Custom user model
-AUTH_USER_MODEL = 'cafe.User'
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Cron jobs
-CRONJOBS = [
-    ('0 */48 * * *', 'django.core.management.call_command', ['cleanup_old_data']),
-]
-
-# #######above is my test local code
-
+# ########above is my inial hosting code code
 
 # """
-# PRODUCTION Settings for Render - pr1_db database
-# Matches local configuration with USE_TZ = True
+# LOCAL Development Settings - Uses Aman database on localhost
 # """
 # import os
 # from pathlib import Path
-# import dj_database_url
 
-# # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# # Build paths
 # BASE_DIR = Path(__file__).resolve().parent.parent
 
-# # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-^#ku(-=1nwd8wnfe&1eri+k)_(8g@!n*#p3&!i4=!!c)&rhl3v')
+# # Security
+# SECRET_KEY = 'django-insecure-^#ku(-=1nwd8wnfe&1eri+k)_(8g@!n*#p3&!i4=!!c)&rhl3v'
+# DEBUG = True
+# ALLOWED_HOSTS = ['*']
 
-# # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-
-# # Better approach for ALLOWED_HOSTS
-# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+# # Timezone - CONSISTENT!
+# USE_TZ = True
+# TIME_ZONE = 'Asia/Kolkata'
 
 # # Application definition
-
 # INSTALLED_APPS = [
 #     'django.contrib.admin',
 #     'django.contrib.auth',
@@ -145,7 +34,6 @@ CRONJOBS = [
 
 # MIDDLEWARE = [
 #     'django.middleware.security.SecurityMiddleware',
-#     'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this for static files
 #     'django.contrib.sessions.middleware.SessionMiddleware',
 #     'django.middleware.common.CommonMiddleware',
 #     'django.middleware.csrf.CsrfViewMiddleware',
@@ -174,19 +62,19 @@ CRONJOBS = [
 
 # WSGI_APPLICATION = 'pr1.wsgi.application'
 
-# # Database
-# # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+# # Database - LOCAL PostgreSQL
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         default='postgresql://postgres:123@localhost:5432/Aman',
-#         conn_max_age=600
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'Aman',              # Local database name
+#         'USER': 'postgres',          # Local DB username
+#         'PASSWORD': '123',           # Local DB password
+#         'HOST': 'localhost',         # Local host
+#         'PORT': '5432',              # PostgreSQL port
+#     }
 # }
 
 # # Password validation
-# # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
-
 # AUTH_PASSWORD_VALIDATORS = [
 #     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
 #     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
@@ -195,36 +83,22 @@ CRONJOBS = [
 # ]
 
 # # Internationalization
-# # https://docs.djangoproject.com/en/4.1/topics/i18n/
-
 # LANGUAGE_CODE = 'en-us'
-
 # TIME_ZONE = 'Asia/Kolkata'
-
 # USE_I18N = True
+# USE_TZ = False  # IMPORTANT: Consistent with top!
 
-# USE_TZ = True
-
-# # Static files (CSS, JavaScript, Images)
-# # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+# # Static files
 # STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# # WhiteNoise configuration for static files
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# # Media files
+# # Media files - LOCAL storage
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # # Custom user model
 # AUTH_USER_MODEL = 'cafe.User'
-
-# # Default primary key field type
-# # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # # Cron jobs
@@ -232,11 +106,137 @@ CRONJOBS = [
 #     ('0 */48 * * *', 'django.core.management.call_command', ['cleanup_old_data']),
 # ]
 
-# # Security settings for production
-# if not DEBUG:
-#     SECURE_SSL_REDIRECT = True
-#     SESSION_COOKIE_SECURE = True
-#     CSRF_COOKIE_SECURE = True
-#     SECURE_BROWSER_XSS_FILTER = True
-#     SECURE_CONTENT_TYPE_NOSNIFF = True
-#     X_FRAME_OPTIONS = 'DENY'
+# #######above is my test local code
+
+
+"""
+PRODUCTION Settings for Render - pr1_db database
+Matches local configuration with USE_TZ = True
+"""
+import os
+from pathlib import Path
+import dj_database_url
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-^#ku(-=1nwd8wnfe&1eri+k)_(8g@!n*#p3&!i4=!!c)&rhl3v')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
+# Better approach for ALLOWED_HOSTS
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'cafe',
+    'django_crontab',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this for static files
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'pr1.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['templates', 'static'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'pr1.wsgi.application'
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://postgres:123@localhost:5432/Aman',
+        conn_max_age=600
+    )
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.1/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'Asia/Kolkata'
+
+USE_I18N = True
+
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# WhiteNoise configuration for static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+# Custom user model
+AUTH_USER_MODEL = 'cafe.User'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Cron jobs
+CRONJOBS = [
+    ('0 */48 * * *', 'django.core.management.call_command', ['cleanup_old_data']),
+]
+
+# Security settings for production
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    X_FRAME_OPTIONS = 'DENY'
